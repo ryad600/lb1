96aadff4161e71cfbebea4016cd8e264f29f3214
Ryad Mohamed <ryad.2002@hotmail.com>
2022-10-27 18:43:13 +0200

    API comments added to delete category and delete product.

--- public/controller/products/delete_product.php
+++ public/controller/products/delete_product.php
@@ -4,12 +4,40 @@
 	use Psr\Http\Message\ServerRequestInterface as Request;
 	use Slim\Factory\AppFactory;
 	
+	/**
+     * @OA\Delete(
+     *     path="/Product/{product/id}",
+     *     summary="Deletes the product with the same product id.",
+     *     tags={Products},
+     *     @OA\Parameter(
+     *         name="product_id",
+     *         in="Product/product_id",
+     *         required=true,
+     *         description="Used to identfy which product the client wants to delete",
+     *         @OA\Schema(
+     *             type="interger",
+     *             example="1"
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Product was succesfully deleted"))
+     * 	   @OA\Response(response="400", description="The client forgotto fill in the text fields"))
+     *     @OA\Response(response="404", description="The Product that the client was searching for was not found."))
+     *     @OA\Response(response="500", description="Internal server error."))
+     * 	   
+     * )
+	 */
+
+
 	$app->delete("/Product/{product_id}", function (Request $request, Response $response, $args) {
 
 		//Check the client's authentication.
 		require "controller/require_authentication.php";
 
+		$args["product_id"] = strip_tags(addslashes($args["product_id"]));
+
+		//Delete the product
 		$product = delete_product($args["product_id"]);
+
 
 		if (is_string($product)) {
 			error($products, 500);
--- public/swagger.php
+++ public/swagger.php
@@ -1,5 +1,5 @@
 <?php
-require("../vendor/autoload.php");
+require "../vendor/autoload.php";
 $openapi = \OpenApi\Generator::scan([__DIR__]);
 header('Content-Type: application/x-yaml');
 echo $openapi->toYaml();
